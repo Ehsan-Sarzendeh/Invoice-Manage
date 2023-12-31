fb11c4ec521b291a6932069111535cd30f26a732
fawzlx <amirhashemi361361@gmail.com>
2023-12-31 14:02:44 +0330

    Add configuration

---
+++ InvoiceManage.App/Services/Infrastructures/ConfigurationExtension.cs
@@ -0,0 +1,24 @@
+﻿using InvoiceManage.App.Forms.Common;
+
+namespace InvoiceManage.App.Services.Infrastructures
+{
+    public static class ConfigurationExtension
+    {
+        public static ParamType? GetParamType(this string section, string input, int type, int pattern)
+        {
+            var paramOption = Program.Configuration
+                .GetSection("StepsConfigurations")
+                .GetSection(section)
+                .GetSection(input)[$"{type}{pattern}"];
+
+            return paramOption switch
+            {
+                "M" => ParamType.Required,
+                "MC" => ParamType.SemiRequired,
+                "A" => ParamType.Optional,
+                "I" => ParamType.Ignore,
+                _ => null
+            };
+        }
+    }
+}
